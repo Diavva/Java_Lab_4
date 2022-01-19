2b4f7e67729a3ee7502258e63d17ac1970d4b6c2
Diavva <Karmasin@interia.pl>
2022-01-19 19:21:31 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,57 @@ public class Main {
         c) stworzyć objekty tych klas i wyświetlić przykładowe wyliczenia
         */
 
+        abstract class Figura {
+            abstract int pole();
+            abstract int obwod();
+            String nazwafigury(String figura){
+                return "To jest figura";
+            }
+        }
+
+        class kwadrat extends Figura {
+
+            int pole() {
+                return 0;
+            }
+
+            int obwod() {
+                return 0;
+            }
+        }
+
+        class prostokat extends  Figura {
+
+            int pole() {
+                return 0;
+            }
+
+            int obwod() {
+                return 0;
+            }
+        }
+
+        Object Kwadrat = new kwadrat(){
+
+            int pole() {
+                return 9;
+            }
+
+            int obwod() {
+                return 12;
+            }
+        };
+
+        Object Prostokat = new prostokat(){
+
+            int pole() {
+                return 20;
+            }
+
+            int obwod() {
+                return 18;
+            }
+        };
+
     }
 }
